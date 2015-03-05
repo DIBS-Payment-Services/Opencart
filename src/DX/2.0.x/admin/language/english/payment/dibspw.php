ff1ac12bb795c38fd9b781f40c7e39f92cb2a7c9
Max White <mbe@ciklum.com>
2015-03-05 18:25:09 +0200

    added sources initial for D2, DX

---
+++ src/DX/2.0.x/admin/language/english/payment/dibspw.php
@@ -0,0 +1,63 @@
+<?php
+    // Heading
+    $_['heading_title']         = 'DIBS Payment Window';
+    $_['text_edit']             = 'Edit DIBS Payment Window';
+                                            
+    // Text                                     
+    $_['text_auto']             = 'Auto';
+    $_['text_standard']         = 'DIBS Payment Window';
+    $_['text_yes']              = 'yes';
+    $_['text_no']               = 'no';
+    $_['text_success']          = 'Success: You have modified DIBS account details!';
+    $_['text_dempty']           = '-'; 
+    $_['text_demail']           = 'Email';
+    $_['text_dpaper']           = 'Paper';
+    $_['text_payment']          = 'Payment';
+    $_['text_da']               = 'Danish';
+    $_['text_nl']               = 'Dutch';
+    $_['text_en']               = 'English';
+    $_['text_fo']               = 'Faroese';
+    $_['text_fi']               = 'Finnish';
+    $_['text_fr']               = 'French';
+    $_['text_de']               = 'German';
+    $_['text_it']               = 'Italian';
+    $_['text_nor']              = 'Norwegian';
+    $_['text_pl']               = 'Polish';
+    $_['text_es']               = 'Spanish';
+    $_['text_sv']               = 'Swedish';
+    $_['text_dibspw']           = '<a onclick="window.open(\'http://dibspayment.com/\');">
+                                       <img src="view/image/payment/dibspw.png" alt="DIBS" title="DIBS" style="border: none;" />
+                                   </a>';
+    $_['text_techsite']         = 'Detailed description of configuration parameters can be found on our <a href="http://tech.dibs.dk" target="_blank">Tech site</a>.';
+    // Entry          
+    $_['entry_status'] 	        = 'Status:';
+    $_['entry_lang'] 	        = 'Language:';
+    $_['entry_default_title']   = 'DIBS Payment Window';
+    $_['entry_text_title']      = 'Title:';
+    $_['entry_testmode']        = 'Testmode:';
+    $_['entry_mid']             = 'Merchant ID:';
+    
+    $_['entry_pid']             = 'Partner ID:';
+	
+    $_['entry_paytype']         = 'Paytype:';
+    $_['entry_default_paytype'] = '';
+    $_['entry_fee']          	= 'Add fee:';
+    $_['entry_capturenow']      = 'Capture now:';
+    $_['entry_sort_order']   	= 'Sort Order:';
+    $_['entry_hmac']          	= 'HMAC:';
+
+    $_['entry_account']         = 'Account:';
+    $_['entry_distr']        	= 'Distribution type:' .
+                                  '<br /><span class="help">' . 
+                                      'Only relevant for invoice payment types (DIBS PW only).' .
+                                  '</span>';
+    $_['entry_order_status_id'] = 'Order Status:';
+    $_['entry_geo_zone_id']        = 'Geo Zone:';
+    $_['entry_total']           = 'Total:<br /><span class="help">' . 
+                                  'The checkout total the order must reach' .
+                                  ' before this payment method becomes active.</span>';
+
+    // Error
+    $_['error_permission']      = 'Warning: You do not have permission to modify payment DIBS!';
+    $_['error_warning']         = 'Warning: Please check the form carefully for errors!';
+    $_['error_mid']             = 'Merchant ID Required!';                                        
